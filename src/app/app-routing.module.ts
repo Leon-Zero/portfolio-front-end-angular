f06d0807fe1280a8e88ce5f39bcdc3070fc532fb
leonardo hidalgo <leon.cba09@gmail.com>
2022-10-12 03:12:01 -0300

    creacion del servicio de guard y uso en las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { GuardService as guard } from './servicios/guard.service';
 
 
 const routes: Routes = [
-  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'portfolio', component: PortfolioComponent, canActivate:[guard], data: {expectedRol: ['admin', 'user']}},
   { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '**', component: PageNotFoundComponent }
